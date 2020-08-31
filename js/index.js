d68e7a6fd821c5a3e15dbdb3b5788ff9a833f231
libinkai <973986577@qq.com>
2020-08-31 21:31:12 +0800

    渲染权限菜单

--- js/index.js
+++ js/index.js
@@ -1,44 +1,160 @@
-$(function (){
+$(function () {
+
+    let $navBoxList =$(".navBox>a")
+    let $itemBoxList =null;
+
     init();
 
-    let $plan =$.Callbacks();
+    let $plan = $.Callbacks();
 
-    $plan.add((_,baseInfo)=>{
+    $plan.add((_, baseInfo) => {
         // console.log(baseInfo)
         // 渲染用户信息和实现退出登录
         $(".baseBox>span").html(`你好，${baseInfo.name || ''}`)
-        
-        $(".baseBox>a").click(async function(){
+
+        $(".baseBox>a").click(async function () {
             let result = await axios.get("/user/signout")
-            if(result.code == 0){
+            if (result.code == 0) {
                 window.location.href = "login.html"
                 return;
             }
             alert("网络不给力，稍后再试")
         });
-    
+
     });
-    $plan.add((power)=>{
+    $plan.add((power) => {
         // console.log("渲染菜单：",power)
+
+
+        let str = ``;
+        if (power.includes("userhandle")) {
+            str += `
+        <div class="itemBox" text="员工管理">
+                <h3>
+                    <i class="iconfont icon-yuangong"></i>
+                    员工管理
+                </h3>
+                <nav class="item">
+                    <a href="page/userlist.html" target="iframeBox">员工列表</a>
+                    <a href="page/useradd.html" target="iframeBox">新增员工</a>
+                </nav>
+            </div>
+        `
+        }
+        if (power.includes("departhandle")) {
+            str += `
+            <div class="itemBox" text="部门管理">
+                <h3>
+                    <i class="iconfont icon-guanliyuan"></i>
+                    部门管理
+                </h3>
+                <nav class="item">
+                    <a href="page/departmentlist.html" target="iframeBox">部门列表</a>
+                    <a href="page/departmentadd.html" target="iframeBox">新增部门</a>
+                </nav>
+            </div>
+            `
+        }
+        if (power.includes("jobhandle")) {
+            str += `
+            <div class="itemBox" text="职位管理">
+                <h3>
+                    <i class="iconfont icon-zhiwuguanli"></i>
+                    职位管理
+                </h3>
+                <nav class="item">
+                    <a href="page/joblist.html" target="iframeBox">职位列表</a>
+                    <a href="page/jobadd.html" target="iframeBox">新增职位</a>
+                </nav>
+            </div>
+            `
+        }
+        if (power.includes("customerall")) {
+            str += `
+            <div class="itemBox" text="客户管理">
+                <h3>
+                    <i class="iconfont icon-kehuguanli"></i>
+                    客户管理
+                </h3>
+                <nav class="item">
+                    <a href="page/customerlist.html" target="iframeBox">我的客户</a>
+                    <a href="page/customerlist.html" target="iframeBox">全部客户</a>
+                    <a href="page/customeradd.html" target="iframeBox">新增客户</a>
+                </nav>
+            </div>
+            `
+        }
+        $(".menuBox").html(str);
+
+        $itemBoxList = $(".nenuBox").find(".itemBox");
     });
 
-    async function init(){
+    function handGroup(index){
+        let $group1 = $itemBoxList.filter((_,item)=>{
+            let text = $(item).attr("text");
+            return text === "客户管理"
+        });
+        let $group2 = $itemBoxList.filter((_,item)=>{
+            let text = $(item).attr("text");
+            return /^(员工管理|部门管理|职位管理)/.test(text)
+        });
+
+        if(index ===0){
+            $group1.css("display","block");
+            $group2.css("display","none");
+        }else if(index ===1){
+            $group1.css("display","none");
+            $group2.css("display","block");
+        }
+    }
+
+    $plan.add((power) =>{
+
+        let initIndex = power.includes("customer") ? 0 : 1;
+        $navBoxList.eq(initIndex).addClass("active").siblings().removeClass("active");
+        handGroup(initIndex)
+        
+        $navBoxList.click(function(){
+            let index = $(this).index();
+            let text = $(this).html().trim();
+            if((text ==="客户管理") && !/customerall/.test(power) || (text === "组织结构") && !/(userhandle|departhandle|jobhandle)/.test(power)){
+                alert("没有访问权限！！！")
+                return
+            }
+            if(index === initIndex) return;
+            $(this).addClass("active").siblings().removeClass("active");
+            handGroup(index);
+            initIndex=index;
+        })
+    })
+
+    // 控制默认iframe中的src
+    $plan.add(power=>{
+        let url = "page/customerlist.html"
+        if(power.includes("customerall")){
+            $(".iframeBox").attr("src",url)
+        }
+    })
+
+
+    async function init() {
         let result = await axios.get("/user/login");
         // console.log(result)
-        if(result.code != 0){
+        if (result.code != 0) {
             alert("你还没有登录，请先登录....")
-            window.location.href="login.html";
+            window.location.href = "login.html";
             return;
         }
 
-        let [power,baseInfo] = await axios.all([
+        let [power, baseInfo] = await axios.all([
             axios.get("/user/power"),
             axios.get("/user/info")
         ])
         // console.log(power)
         // console.log(baseInfo)
-        baseInfo.code ===0 ? baseInfo = baseInfo.data : null;
-        $plan.fire(power,baseInfo)
+        power.code === 0 ? power = power.power : null;
+        baseInfo.code === 0 ? baseInfo = baseInfo.data : null;
+        $plan.fire(power, baseInfo)
 
     }
 })
